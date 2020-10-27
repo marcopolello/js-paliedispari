877dd7abc97714125219d9160fad40791246fbc9
marcopolello <marco-pol@hotmail.it>
2020-10-27 15:44:32 +0100

    rovescia la parola

--- js/script.js
+++ js/script.js
@@ -10,18 +10,16 @@ console.log(inputUtente);
 // 1 step: la mia funzione avrà il compito di rovesciare la parola data dall'utente
 // 2 step: analizzare la parola invertita e paragonarla alla parola data dall'input
 // 3 step: se le parole combaciano dara una stampa, se non è cosi una stampa diversa
+var outPutFunct = rovescia(inputUtente);
+console.log(outPutFunct);
+
 function rovescia(input)
 {
   var parolaRovesciata = "";
-  var i = input.length;
-  while (i >= 0)
-  {
-    parolaRovesciata = parolaRovesciata + input[i]
-    i--
+  var i = input.length - 1;
+  while (i >= 0) {
+    parolaRovesciata = parolaRovesciata + input[i];
+    i--;
   }
-  console.log(parolaRovesciata);
   return parolaRovesciata;
 }
-
-var outPutFunct = rovescia(inputUtente);
-console.log(outPutFunct);
